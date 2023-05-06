131d482cdcd04560c5dac27ae0c129bd274e6478
Артем <artemgolyshkov@mail.ru>
2023-05-06 12:28:42 +0300

    Домашка 2.3

---
+++ src/main/java/pro/sky/calculatorskypro/CalculatorSkyproApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.calculatorskypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorSkyproApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CalculatorSkyproApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pro/sky/calculatorskypro/HalloCalculator.java
@@ -0,0 +1,7 @@
+package pro.sky.calculatorskypro;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HalloCalculator {
+}
